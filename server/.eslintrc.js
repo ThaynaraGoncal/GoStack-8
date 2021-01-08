d099275042b6f77f60281f611670b368a39d10c3
thaynaragoncal <thaynarafaria21@gmail.com>
2021-01-08 15:58:50 -0300

    feito model user

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,17 +3,17 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
+    'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'netx' }],
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
